f76900630197808771fea28caaa4d504e4074691
Sébastien Deleuze <seb@deleuze.fr>
2012-05-14 16:42:11 +0200

    Use setConfigLocations API instead of initParam to define applicationContext locations. Add Data intializer thanks to @PostInitialize

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -1,13 +1,23 @@
 package todo;
 
 import javax.inject.Inject;
+import org.resthub.common.util.PostInitialize;
 import org.resthub.web.controller.RepositoryBasedRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller @RequestMapping("/api/todo")
 public class TodoController extends RepositoryBasedRestController<Todo, String, TodoRepository> {
 
+    protected Logger logger = LoggerFactory.getLogger(TodoController.class);
+    
+    @PostInitialize
+    public void init() {
+        this.repository.save(new Todo("test123"));
+    }
+    
     @Inject
     @Override
     public void setRepository(TodoRepository repository) {
--- src/main/java/todo/WebAppInitializer.java
+++ src/main/java/todo/WebAppInitializer.java
@@ -1,10 +1,8 @@
 package todo;
 
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.XmlWebApplicationContext;
@@ -16,13 +14,14 @@ public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
                 
         XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        String[] locations = {"classpath*:resthubContext.xml", "classpath*:applicationContext.xml"};
+        appContext.setConfigLocations(locations);
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/*");
         
         servletContext.addListener(new ContextLoaderListener(appContext));
-        servletContext.setInitParameter("contextConfigLocation", "classpath*:resthubContext.xml classpath*:applicationContext.xml");
 
     }
 }
