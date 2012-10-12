26a7546712fb659a365a9fa73b35532e034eb883
Brian Clozel <brian.clozel@atos.net>
2012-10-12 19:30:36 +0200

    use webappconfigurer instead of applicationContext file

---
+++ src/main/java/todo/WebAppConfigurer.java
@@ -0,0 +1,12 @@
+package todo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories("todo")
+@ImportResource("classpath*:applicationContext.xml")
+public class WebAppConfigurer {
+
+}
