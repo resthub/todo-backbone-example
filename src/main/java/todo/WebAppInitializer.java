aa06bf7a1a67ce056b441bf868ddb9497d491c5a
JRI <julien.ripault@atos.net>
2014-09-17 16:15:27 +0200

    Add support for swagger 0.8.8 + add hikariCP profile

--- src/main/java/todo/WebAppInitializer.java
+++ src/main/java/todo/WebAppInitializer.java
@@ -17,7 +17,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.getEnvironment().setActiveProfiles("resthub-jpa", "resthub-pool-bonecp", "resthub-web-server", "resthub-client-logging");
+        appContext.getEnvironment().setActiveProfiles("resthub-jpa", "resthub-pool-hikaricp", "resthub-web-server", "resthub-client-logging");
         String[] locations = { "classpath*:resthubContext.xml", "classpath*:applicationContext.xml" };
         appContext.setConfigLocations(locations);
 
